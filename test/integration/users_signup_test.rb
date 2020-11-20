cfcbe41986ea2793f624ae95dea870b37f3ce3c4
Tchander <evgeniy.tchander@yandex.ru>
2020-11-20 18:19:22 +0300

    End of the chapter 12.1

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,10 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "",
                                          email: "user@invalid",
-                                         password:
-                                             "foo",
-                                         password_confirmation:
-                                             "bar" } }
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
     end
     assert_template 'users/new'
   end
@@ -23,12 +21,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User",
-                                         email:
-                                             "user@example.com",
-                                         password:
-                                             "password",
-                                         password_confirmation:
-                                             "password" } }
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
     end
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
